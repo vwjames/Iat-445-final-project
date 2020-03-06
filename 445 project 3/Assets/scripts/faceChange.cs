8ebc6c8761e4cd4d478db884f1d6fab8eaf03107
vwjames <vwjames@sfu.ca>
2020-03-05 16:21:09 -0800

    added drone facechange script

---
+++ 445 project 3/Assets/scripts/faceChange.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class faceChange : MonoBehaviour
+{
+    public int Faceset = 0;
+    public float distance = 0.25f;
+    public float startx = 0.26f;
+    public float starty = 0.21f;
+    public Material droneface;
+    void Update()
+    {
+        droneface.mainTextureOffset = new Vector2 (startx+Faceset*distance, starty);
+    }
+}
